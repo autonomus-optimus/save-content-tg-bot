56f8dc0027fcd74bd600b24cccc7900fd1e19770
autonomus-optimus <116957912+autonomus-optimus@users.noreply.github.com>
2023-07-02 10:39:45 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 26957450
+API_HASH = "c82248cee35b0c9a9de4a5caf0c2ac9f"
+BOT_TOKEN = "5465761249:AAFYGRwBvPMTlH-OVlGPbH1RpSzpLnKhR4I"
+SESSION = "AgAjAXdJu9EJDSdbvwqfztbBfhOb54z0j_uSU_eI3g9kUKLeRoRVz89bVV10dFIxSQLTSGIzb9tpSlY99xNEqyJ6LeyM9t46fuqiMydlqe8hMnnIa7Kl484E565Hpx9IVUqbWwNOSGPO_PfOY77Jyw27j8mnjKLDuVgHf-k1gLGW8BioQPEDvIlAn0GX6VAvSqt_FljK-QqrYcbq9d0InPzhNnPVTRh9uGnMlltFQnp6siVZcmDNO5Xu52COyZFkE-iR-_LBx9OjzAXNbGDG3TSw4u8ZIRdDgdhoVFYC5u8NgWRCTBCPMphLn31_GKHYRDgceLAqU7k3LfhJoVAcI_UWAAAAATvYpM4A"
+FORCESUB = "savecontenttempbot"
+AUTH = 5299021006
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
